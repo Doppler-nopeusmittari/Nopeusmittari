ded06ede087d939c642b08c045623ba3402c5943
JKauppilaa <194209395+JKauppilaa@users.noreply.github.com>
2026-04-09 13:22:44 +0300

    Yhdistetty ble_hb100 ja Tennis_tutka koodit

--- Tennis_tutka/main/main.c
+++ Tennis_tutka/main/main.c
@@ -5,6 +5,8 @@
 #include "esp_adc/adc_cali.h" //Tarvitaan kalibrointiin
 #include "esp_adc/adc_cali_scheme.h"
 #include "dsps_fft2r.h" // Espressifin laitteistokiihdytetty dsp kirjasto
+#include "ble_hb100.h"
+#include "nvs_flash.h"
 
 //Globaali "kahva", josta voidaan tarttua ADC "apulaiseen"
 adc_continuous_handle_t tutka_adc_kahva = NULL;
@@ -12,9 +14,10 @@ adc_continuous_handle_t tutka_adc_kahva = NULL;
 adc_cali_handle_t kalibrointi_kahva = NULL;
 
 //Taulukko, johon "apulainen" tuo keräämänsä datan (koko aiemmin määritelty 2048 tavua)
-    uint8_t tulopuskuri[2048]; //2048 pieniä, yhden tavun kokoisia muistilokeroita
-    //Taulukko, johon for silmukassa puhdistetut jännitearvot tallennetaan FFT varten
-    float fft_taulukko[1024];
+uint8_t tulopuskuri[2048]; //2048 pieniä, yhden tavun kokoisia muistilokeroita
+//Taulukko, johon for silmukassa puhdistetut jännitearvot tallennetaan FFT varten
+float fft_taulukko[1024];
+
 
 
 //Funktio, joka palkkaa ja opettaa apulaisen
@@ -62,6 +65,11 @@ void alusta_tutka() {
 }
 
 void app_main(void) {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        nvs_flash_erase();
+        nvs_flash_init();
+    }
 
     printf("Tutkaohjelma käynnistyy!\n");
 
@@ -72,11 +80,12 @@ void app_main(void) {
     alusta_tutka();
 
     adc_continuous_start(tutka_adc_kahva);
+    ble_hb100_init();
 
     
 
     while (1) {
-        printf("Tutka pyörii..\n");
+        //printf("Tutka pyörii..\n");
         //Muuttuja "kuitille" johon "apulainen" kertoo, kuinka monta tavua se onnistui laittamaan laatikkoon
         uint32_t luettu_maara = 0;
 
@@ -88,7 +97,7 @@ void app_main(void) {
 
         //tarkistetaan, onko tila ok
         if (tila == ESP_OK) {
-            printf("Dataa haettu onnistuneesti. Saimme %lu tavua.\n", luettu_maara);
+            //printf("Dataa haettu onnistuneesti. Saimme %lu tavua.\n", luettu_maara);
 
             for (int i = 0; i < luettu_maara; i +=4) {
                 //Osoitetaan kirjeenavaajalle oikea kohta laatikosta
@@ -111,10 +120,11 @@ void app_main(void) {
                 fft_taulukko[i/2] = (float)jannite_mv;
                 //Tallennetaan nolla imaginaariosaan (heti seuraavaan lokeroon)
                 fft_taulukko[(i/2) + 1] = 0;
-            
+                /*
                 if (i == 0) {
                 printf("Jännite: %d mV\n", jannite_mv);
                 }
+                */
         }
             //Laskentakäsky
             dsps_fft2r_fc32(fft_taulukko, 512);
@@ -146,19 +156,20 @@ void app_main(void) {
             //Tämä luku on siis tutkan tarkkuus eli resoluutio. Jokainen lokero FFT taulukossa edustaa siis noin 39Hz
             //askelta. Lokero 1 on 39Hz, Lokero 2 78Hz...
              
+            if(suurin_voimakkuus > 50000.0){
+                //Muutetaan indeksi taajuudeksi ja taajuus nopeudeksi
+                //Saimme edellisessä osiossa voimaikkaimman signaalin lokeron (paras_indeksi). Voimme käyttää tätä taajuuden saamiseksi
+                float taajuus_hz = (float)paras_indeksi * (20000.0 / 512.0);
+                //Muunnetaan taajuus nopeudeksi tutkakohtaisella vakiolla 19.49. Tämä tarkoittaa, että 19,49Hz vastaa 1km/h
+                //vakio saadaan kaavalla Fd = 2*nopeus*tutkan taajuus/valonnopeus. Jos käytetään nopeutena 1 m/s ja tutkan taajuutta 10,525GHz
+                //saadaan tulos 2*1*10525000000/300000000 = 70,16Hz. Tämä muutetaan km/h jakamalla tämä 3,6 = 19,488...
+                float nopeus_kmh = taajuus_hz / 19.49;
+                //printf("Suurin voimakkuus: %.2f\n", suurin_voimakkuus);
 
-            //Muutetaan indeksi taajuudeksi ja taajuus nopeudeksi
-            //Saimme edellisessä osiossa voimaikkaimman signaalin lokeron (paras_indeksi). Voimme käyttää tätä taajuuden saamiseksi
-            float taajuus_hz = (float)paras_indeksi * (20000.0 / 512.0);
-            //Muunnetaan taajuus nopeudeksi tutkakohtaisella vakiolla 19.49. Tämä tarkoittaa, että 19,49Hz vastaa 1km/h
-            //vakio saadaan kaavalla Fd = 2*nopeus*tutkan taajuus/valonnopeus. Jos käytetään nopeutena 1 m/s ja tutkan taajuutta 10,525GHz
-            //saadaan tulos 2*1*10525000000/300000000 = 70,16Hz. Tämä muutetaan km/h jakamalla tämä 3,6 = 19,488...
-            float nopeus_kmh = taajuus_hz / 19.49;
-            printf("Suurin voimakkuus: %.2f\n", suurin_voimakkuus);
-
-            //Tulostetaan tulos yhden desimaalin tarkkuudella
-            printf("Nopeus: %.1f km/h\n", nopeus_kmh);
-            
+                //Tulostetaan tulos yhden desimaalin tarkkuudella
+                printf("Nopeus: %.1f km/h\n", nopeus_kmh);
+                ble_notify_ball_speed(nopeus_kmh);
+            }
         } else {
             printf ("Datan hakemisessa oli ongelma.\n");
         }
